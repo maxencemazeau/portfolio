10e1ce922e5f14b53d812ef8f965334e071778e2
Maxence Maze au <maxencemazeau@Maxences-MacBook-Air.local>
2023-07-10 18:11:40 +0200

    First

---
+++ src/page/home.js
@@ -0,0 +1,12 @@
+import React from "react"
+import NombreProjet from "../component/NombreProjet"
+
+export default function Home(){
+
+
+    return(
+        <>
+            <NombreProjet />
+        </>
+    )
+}
