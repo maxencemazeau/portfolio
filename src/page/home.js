81d71ac74ab7970620d1499beeda43361fc0d834
Maxence Maze au <maxencemazeau@Maxences-MacBook-Air.local>
2023-07-16 15:00:58 +0200

    Header responsive

--- src/page/home.js
+++ src/page/home.js
@@ -1,21 +1,28 @@
-import React from "react"
-import NombreProjet from "../component/NombreProjet"
-import Header from "../component/Header"
+import React, { useState } from "react";
+import NombreProjet from "../component/NombreProjet";
+import Header from "../component/Header";
+import Projet from "../component/Projet";
 
-export default function Home(){
+export default function Home() {
+  const [headerHeight, setHeaderHeight] = useState("h-16");
 
+  const handleHeaderHeight = (isExpanded) => {
+    if (isExpanded) {
+      setHeaderHeight("h-64");
+    } else {
+      setHeaderHeight("h-16");
+    }
+  };
 
-    return(
-        <>
-        <div class="grid grid-cols-1 gap-4 lg:grid-cols-4 lg:gap-8">
-            <div class="h-32 rounded-lg bg-gray-100">
-                <Header />
-            </div>
-            <div class="h-32 rounded-lg lg:col-span-3">
-                <NombreProjet />
-            </div>
-        </div>          
-           
-        </>
-    )
+  return (
+    <div className="flex flex-col lg:flex-row">
+      <div className={`rounded-lg bg-gray-100 lg:w-64 ${headerHeight}`}>
+        <Header handleHeight={handleHeaderHeight} />
+      </div>
+      <div className="flex flex-col flex-1 lg:ml-8">
+        <NombreProjet />
+        <Projet />
+      </div>
+    </div>
+  );
 }
