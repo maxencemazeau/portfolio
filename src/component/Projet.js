123821b219175968c14b71bd22fb8a7d0e9f9263
Maxence Maze au <maxencemazeau@Maxences-MacBook-Air.local>
2023-08-04 18:59:53 +0200

    Ajout des projets

--- src/component/Projet.js
+++ src/component/Projet.js
@@ -5,37 +5,18 @@ export default function Projet () {
             title: "Epée Cégep de Rivère du Loup - Allaitement Quebec",
             desc: "Extension du site web d'Allaitement avec mise en place d'un chat live, d'inscription à des activités et automatisations d'emails. Réalisé dans une équipe de 3 développeurs.",
             img: "allaitement.png",
-            authorName: "Technologies",
+            authorName: "Professionnel",
             date: "ReactJS / NodeJS / MySQL",
             href: "https://chat.allaitementquebec.org/Connexion"
         },
         {
-            title: "A Quick Guide to WordPress Hosting",
-            desc: "According to him, â€œI'm still surprised that this has happened. But we are surprised because we are so surprised.â€More revelations about Whittington will be featured in the film",
-            img: "https://images.unsplash.com/photo-1620287341056-49a2f1ab2fdc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-            authorLogo: "https://api.uifaces.co/our-content/donated/FJkauyEa.jpg",
-            authorName: "Micheal",
-            date: "Jan 4 2022",
-            href: ""
+            title: "Site vitrine Hestenn",
+            desc: "Création et intégration de template pour un site vitrine one page pour entreprise.",
+            img: "hestenn.png",
+            authorName: "Personnel",
+            date: "Html / CSS / Javascript",
+            href: "www.hestenn.com"
         },
-        {
-            title: "7 Promising VS Code Extensions Introduced in 2022",
-            desc: "I hope I remembered all the stuff that they needed to know. They're like, 'okay,' and write it in their little reading notebooks. I realized today that I have all this stuff that",
-            img: "https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-            authorLogo: "https://randomuser.me/api/portraits/men/46.jpg",
-            authorName: "Luis",
-            date: "Jan 4 2022",
-            href: ""
-        },
-        {
-            title: "How to Use Root C++ Interpreter Shell to Write C++ Programs",
-            desc: "The powerful gravity waves resulting from the impact of the planets' moons â€” four in total â€” were finally resolved in 2015 when gravitational microlensing was used to observe the",
-            img: "https://images.unsplash.com/photo-1617529497471-9218633199c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-            authorLogo: "https://api.uifaces.co/our-content/donated/KtCFjlD4.jpg",
-            authorName: "Lourin",
-            date: "Jan 4 2022",
-            href: ""
-        }
     ]
     
     return (
