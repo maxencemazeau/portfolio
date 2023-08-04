ce2625bfd45a19591907df459ead87db233454f9
Maxence Maze au <maxencemazeau@Maxences-MacBook-Air.local>
2023-08-04 19:51:48 +0200

    Target A projet

--- src/component/Projet.js
+++ src/component/Projet.js
@@ -15,7 +15,7 @@ export default function Projet () {
             img: "hestenn.png",
             authorName: "Personnel",
             date: "Html / CSS / Javascript",
-            href: "www.hestenn.com"
+            href: "https://www.hestenn.com/"
         },
     ]
     
@@ -25,7 +25,7 @@ export default function Projet () {
                 {
                     posts.map((items, key) => (
                         <article className="max-w-md mx-auto mt-4 shadow-lg border rounded-md duration-300 hover:shadow-sm" key={key}>
-                            <a href={items.href}>
+                            <a href={items.href} target="_blank">
                                 <img src={items.img} loading="lazy" alt={items.title}  className="w-full h-48 rounded-t-md" />
                                 <div className="flex items-center mt-2 ml-4 pt-3 mr-2">                                   
                                     <div className="">
